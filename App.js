a72d687ddebf2887f83a7961f251eafa371151ca
Reinaldo Trindade <rewifetri@gmail.com>
2017-11-27 11:17:03 -0200

    Project TODO List

--- App.js
+++ App.js
@@ -1,5 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+/*
+
+Specific Requirements
+
+    Use create-react-native-app to build your project.
+    Allow users to create a deck which can hold an unlimited number of cards.
+    Allow users to add a card to a specific deck.
+    The front of the card should display the question.
+    The back of the card should display the answer.
+    Users should be able to quiz themselves on a specific deck and receive a score once they're done.
+    Users should receive a notification to remind themselves to study if they haven't already for that day.
+
+
+
+TODO:
+
+
+    Deck List View (Default View)
+        displays the title of each Deck
+        displays the number of cards in each deck
+*/
 
 export default class App extends React.Component {
   render() {
